c165815925360bc11aa40af94ab068a519a15263
alxevvv <alxevvv@gmail.com>
2024-08-04 02:16:18 +0200

    method for merging dataclasses

--- mergr/merger.py
+++ mergr/merger.py
@@ -1,8 +1,14 @@
 from collections.abc import Callable, Mapping, Sequence
-from typing import Any, ClassVar, TypeAlias
+from dataclasses import replace
+from typing import Any, ClassVar, Protocol, TypeAlias, runtime_checkable
 
 MergeFn: TypeAlias = Callable[[Any, Any], Any]
 Strategies: TypeAlias = dict[type | tuple[type, ...], MergeFn]
+
+
+@runtime_checkable
+class Dataclass(Protocol):
+    __dataclass_fields__: dict[str, Any]
 
 
 class Merger:
@@ -51,6 +57,17 @@ class Merger:
     def merge_sequences(self, a: Sequence, b: Sequence) -> list:
         return [*a, *b]
 
+    def merge_dataclasses(self, a: Dataclass, b: Dataclass) -> Dataclass:
+        updates = {}
+        for field in a.__dataclass_fields__.values():
+            if hasattr(b, field.name):
+                value_a = getattr(a, field.name)
+                value_b = getattr(b, field.name)
+                updates[field.name] = self.merge(value_a, value_b)
+        if len(updates):
+            return replace(a, **updates)  # type: ignore[type-var]
+        return a
+
     def merge(self, a: Any, b: Any) -> Any:
         if type(a) is str or type(a) is bytes:
             return self.pick_and_merge(a, b)
@@ -60,5 +77,8 @@ class Merger:
 
         if isinstance(a, Sequence) and isinstance(b, Sequence):
             return self.merge_sequences(a, b)
+
+        if isinstance(a, Dataclass) and isinstance(b, Dataclass):
+            return self.merge_dataclasses(a, b)
 
         return self.pick_and_merge(a, b)
